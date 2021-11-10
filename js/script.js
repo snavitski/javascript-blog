883813527aff46090db915a2db28185d119bbabd
Siarhei Navitski <sergiej.nowicki@gmail.com>
2021-11-10 10:03:16 +0100

    add class active js

--- js/script.js
+++ js/script.js
@@ -4,34 +4,35 @@
     console.log('links:', links);
   });*/
 const titleClickHandler = function () {
+	event.preventDefault();
 	const clickedElement = this;
-
 	console.log("Link was clicked!");
 
-	/* remove class 'active' from all article links  */
+	/* [DONE] remove class 'active' from all article links  */
 	const activeLinks = document.querySelectorAll(".titles a.active");
 
 	for (let activeLink of activeLinks) {
 		activeLink.classList.remove("active");
 	}
 
-	/* add class 'active' to the clicked link */
+	/* [In PROGRESS] add class 'active' to the clicked link */
 	console.log("clickedElement:", clickedElement);
-	console.log("clickedElement (with plus): " + clickedElement);
-	clickedElement.classList.add("active");
 
-
-	/* remove class 'active' from all articles */
+	/* [DONE] remove class 'active' from all articles */
 	const activeArticles = document.querySelectorAll(".post a.active");
 	for (let activeArticle of activeArticles) {
 		activeArticle.classList.remove("active");
 	}
 
 	/* get 'href' attribute from the clicked link */
+	const articleSelector = clickedElement.getAttribute("href");
+	console.log(articleSelector);
 
 	/* find the correct article using the selector (value of 'href' attribute) */
+	const activeArticle = document.querySelector(articleSelector);
 
 	/* add class 'active' to the correct article */
+	activeArticle.classList.add("active");
 };
 
 const links = document.querySelectorAll(".titles a");
