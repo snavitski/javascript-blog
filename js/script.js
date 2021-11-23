f8cb83a086e47807faa8565c5f55f056d3c46e3a
Siarhei Navitski <sergiej.nowicki@gmail.com>
2021-11-23 09:38:00 +0100

    generateAuthor

--- js/script.js
+++ js/script.js
@@ -38,7 +38,9 @@ const titleClickHandler = function () {
 const optArticleSelector = ".post",
 	optTitleSelector = ".post-title",
 	optTitleListSelector = ".titles",
-	optArticleTagsSelector = ".post-tags .list";
+	optArticleTagsSelector = ".post-tags .list",
+	optArticleAuthorSelector = ".post-author",
+	optTagsListSelector = ".tags .list";
 
 function generateTitleLinks() {
 	/* remove contents of titleList */
@@ -95,9 +97,10 @@ function generateTags() {
 		console.log(articleTagsArray);
 		/* START LOOP: for each tag */
 		for (let tag of articleTagsArray) {
-		/* generate HTML of the link */
-		const tagLinkHtml = '<li><a href="#tag-' + tag + '"><span>' + tag + "</span></a></li>";
-		/* add generated code to html variable */
+			/* generate HTML of the link */
+			const tagLinkHtml =
+				'<li><a href="#tag-' + tag + '"><span>' + tag + "</span></a></li>";
+			/* add generated code to html variable */
 			html = html + tagLinkHtml;
 			console.log(tagLinkHtml);
 		}
@@ -107,6 +110,79 @@ function generateTags() {
 		console.log(tagLinks);
 	}
 }
-
 generateTags();
 
+function generateAuthors() {
+	const articles = document.querySelectorAll(optArticleSelector);
+	for (let article of articles) {
+		const titleList = article.querySelector(optArticleTagsSelector);
+		const authorWrapper = article.querySelector(optArticleTagsSelector);
+		let html = "";
+		const articleAuthors = article.getAttribute("data-author");
+		const linkHTML =
+			'<a href="#author-' + articleAuthors + '">' + articleAuthors + "</a>";
+		html = html + linkHTML;
+		console.log(html);
+		authorWrapper.innerHTML = html;
+	}
+}
+generateAuthors();
+
+function tagClickHandler(event) {
+	/* prevent default action for this event */
+	event.preventDefault();
+
+	/* make new constant named "clickedElement" and give it the value of "this" */
+	const clickedElement = this;
+	/* make a new constant "href" and read the attribute "href" of the clicked element */
+	const href = clickedElement.getAttribute("href");
+	/* make a new constant "tag" and extract tag from the "href" constant */
+	const tag = href.replace("#tag-", "");
+	/* find all tag links with class active */
+	const tagLinks = document.querySelectorAll('a.active[href^="#tag-"]');
+	/* START LOOP: for each active tag link */
+	for (let tagLink of tagLinks) {
+		/* remove class active */
+		tagLink.classList.remove("active");
+		/* END LOOP: for each active tag link */
+	}
+	/* find all tag links with "href" attribute equal to the "href" constant */
+	const tagLinksHref = document.querySelectorAll('a.[href="' + href + '"]');
+	/* START LOOP: for each found tag link */
+	for (let tagLinkHref of tagLinksHref) {
+		/* add class active */
+		tagLinkHref.classList.add("active");
+		/* END LOOP: for each found tag link */
+	}
+	/* execute function "generateTitleLinks" with article selector as argument */
+	generateTitleLinks('[data-tags~="' + tag + '"]');
+}
+function addClickListenersToTags() {
+	/* find all links to tags */
+	const allLinksToTags = document.querySelectorAll('a[herf^="#tag-"]');
+	console.log(allLinksToTags);
+	/* START LOOP: for each link */
+	for (let link of allLinksToTags) {
+		/* add tagClickHandler as event listener for that link */
+		link.addEventListener("click", tagClickHandler);
+		/* END LOOP: for each link */
+	}
+}
+addClickListenersToTags();
+
+function authorClickHandler(event) {
+	event.preventDefault();
+	const clickedElement = this;
+	const href = clickedElement.getAttribute("href");
+	console.log(href);
+	const tag = href.replace("#author-", "");
+	const authorLinks = document.querySelectorAll('a.active[href^=#author-"]');
+	for (let authorLink of authorLinks) {
+		authorLink.classList.remove("active");
+	}
+	const authorLinksHref = document.querySelectorAll('a[href="' + href + '"]');
+	for (let authorLinkHref of authorLinksHref) {
+		authorLinkHref.classList.add("active");
+	}
+	generateTitleLinks('[data-author="' + tag + '"]');
+}
